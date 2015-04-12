5af3398416530e7d9b57fbce50135f7437bcfae6
oscnet <oscnet@163.com>
2015-04-12 13:06:18 +0800

    export create-captcha-png function

--- project.clj
+++ project.clj
@@ -7,6 +7,5 @@
                  [ring/ring-core "1.3.2"]
                  [org.clojars.smallrivers/simplecaptcha "1.2.1"]]
   :profiles {:test {:dependencies [[ring/ring-defaults "0.1.4"]
-				   [compojure "1.3.3"]
+                                   [compojure "1.3.3"]
                                    [ring/ring-mock "0.2.0"]]}})
-
--- src/clj_scaptcha/core.clj
+++ src/clj_scaptcha/core.clj
@@ -1,26 +1,23 @@
 (ns clj-scaptcha.core
   (:import nl.captcha.Captcha
+           java.io.PipedOutputStream
            javax.imageio.ImageIO)
   (:require [ring.util.io :refer [piped-input-stream]]
             [ring.util.response :refer [response header content-type]]))
 
-(defn- get-captcha [req]
-  (get-in req [:session :captcha]))
-
 (defn correct? [answer req]
-  (if-let [^Captcha captcha (get-captcha req)]
+  (if-let [^Captcha captcha (get-in req [:session :captcha])]
     (.isCorrect captcha answer)))
 
-(defn create-captcha [width height]
+(defn- create-captcha [width height]
   (let [captcha (new nl.captcha.Captcha$Builder width height)]
     (-> captcha
-      (.addText)
-      (.gimp)
-      (.addNoise)
-      (.addNoise)
-      ;(.addBorder)
-      (.addBackground)
-      (.build))))
+        (.addText)
+        (.gimp)
+        (.addNoise)
+        (.addNoise)
+        (.addBackground)
+        (.build))))
 
 (defn- servlet-context
   [req]
@@ -28,18 +25,19 @@
     (try (.getContextPath context)
       (catch IllegalArgumentException _ context))))
 
+(defn create-captcha-png [request width height]
+  (let [^Captcha captcha (create-captcha width height)
+        image (.getImage captcha)]
+    (->
+     (piped-input-stream #(ImageIO/write image "png" ^PipedOutputStream %))
+     response
+     (header "Cache-Control" "private,no-cache,no-store")
+     (content-type "image/png")
+     (assoc :session (assoc (:session request) :captcha captcha)))))
+
 (defn wrap-captcha [handler & [{:keys [path width height] :or {path "/captcha" width 200 height 50}}]]
   (fn [request]
     (if (and (= (:uri request) (str (servlet-context request) path))
              (= (:request-method request) :get))
-      (let [^Captcha captcha (create-captcha width height)
-            img (.getImage captcha)]
-        (->
-         (piped-input-stream
-          (fn [output]
-            (ImageIO/write img "png" output)))
-         response
-         (header "Cache-Control" "private,no-cache,no-store")
-         (content-type "image/png")
-         (assoc :session (assoc (:session request) :captcha captcha))))
+      (create-captcha-png request width height)
       (handler request))))
