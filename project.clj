7909be5fa926c22db6aa9d3eb562f08f94ad7b84
oscar <oscnet@163.com>
2015-04-12 09:53:15 +0800

    mv compojure deps to test

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [compojure "1.3.3"]
                  [ring/ring-core "1.3.2"]
                  [org.clojars.smallrivers/simplecaptcha "1.2.1"]]
   :profiles {:test {:dependencies [[ring/ring-defaults "0.1.4"]
+				   [compojure "1.3.3"]
                                    [ring/ring-mock "0.2.0"]]}})
 
